813cd13cc50c4bdd135bd466662ef41e6cc4caa8
shailajaBegari <begari.shailaja21@gmail.com>
2021-12-11 10:05:39 +0530

    commit

--- script1.js
+++ script1.js
@@ -1,13 +1,4 @@
 
-// // input
-// var username = prompt("Give me username")
-
-// // processing
-// var welcomeMessage = "this script works!! " + username;
-
-// // output
-// alert(welcomeMessage);
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
